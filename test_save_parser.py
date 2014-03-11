9426fdbf6e260319038e2f67deb3d4321aed7540
Matthew Lewis <matt@mplewis.com>
2014-03-10 20:17:27 -0500

    Restructure again; add working tests

---
+++ test_save_parser.py
@@ -0,0 +1,10 @@
+import sure  # noqa
+from save_parser import SaveDataGen1
+
+with open('test_save.sav', 'rb') as f:
+    save = SaveDataGen1(f.read())
+
+save.money.should.equal(40398)
+save.trainer_name.should.equal('RED')
+save.trainer_id.should.equal(20152)
+save.rival_name.should.equal('BLUE')
